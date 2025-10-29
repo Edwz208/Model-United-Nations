6cb43ca0b1f2a6b58362cf7ddf0d87abd2f3be48
Edwin Z <147886945+Edwz208@users.noreply.github.com>
2025-10-29 12:28:34 -0400

    formatting and simplifying login logic

--- routers/login.py
+++ routers/login.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, status, HTTPException, Response, Request
-from authentication import verify, decode, generateJwt, roleList,SECRET_KEY, REFRESH_KEY, REFRESH_TOKEN_EXPIRE_MINUTES, ACCESS_TOKEN_EXPIRE_MINUTES
+from authentication import decode, generateJwt, SECRET_KEY, REFRESH_KEY, REFRESH_TOKEN_EXPIRE_MINUTES, ACCESS_TOKEN_EXPIRE_MINUTES
 from schemas import User
 from db import get_async_pool
 from psycopg.rows import dict_row
-import asyncio
 router = APIRouter()
 from .amendmentsData import getOwnAmendments, getRecentAmendments
 
@@ -11,44 +10,31 @@ pool = get_async_pool()
 
 
 @router.post("/login", status_code=status.HTTP_202_ACCEPTED)
-async def login(user: User, response: Response):
+async def login(user: User, response: Response) -> dict:
     async with pool.connection() as conn:
         async with conn.cursor(row_factory=dict_row) as cursor:
-            user.country = user.country.lower().capitalize()
-            print(user.code)
             await cursor.execute(
                 """SELECT login, country, role, id from delegates WHERE country = %s""",
-                (user.country,),
+                (user.country.lower().capitalize(),),
             )
             returned_info = await cursor.fetchone()
-
-            if not returned_info or (not user.code==returned_info["login"]):
+            if not returned_info or (not user.code == returned_info["login"]):
                 raise HTTPException(
                     status_code=status.HTTP_404_NOT_FOUND,
                     detail="Invalid credes",
-                )# removed hash check for now
-            role = returned_info["role"]
-            print(role)
-            returned_info["roles"] = [role]
+                ) # removed hash check for now
             
-            id = returned_info["id"]
-            recentAmendments = asyncio.create_task(getRecentAmendments()) 
-            del returned_info["role"]
             del returned_info["login"]
             returned_info.update({"accessToken": generateJwt(returned_info, SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES)})
-            response.set_cookie(key="refresh_token",value=f"Bearer {generateJwt(returned_info, REFRESH_KEY, REFRESH_TOKEN_EXPIRE_MINUTES)}", httponly=True, secure=False, samesite="lax", path="/refresh")
-            if roleList.get("member") in returned_info["roles"]:
-                ownAmendments = asyncio.create_task(getOwnAmendments(id))
-                returned_info["ownAmendments"] = await ownAmendments
-                returned_info["recentAmendments"] = await recentAmendments
-            elif roleList.get("admin") in returned_info['roles']:
-                returned_info["recentAmendments"] = await recentAmendments
+            response.set_cookie(key="refresh_token",value=generateJwt(returned_info, REFRESH_KEY, REFRESH_TOKEN_EXPIRE_MINUTES), httponly=True, secure=False, samesite="lax", path="/refresh")
+            returned_info["recentAmendments"] = await getRecentAmendments()
+            returned_info["ownAmendments"] = await getOwnAmendments(returned_info["id"])
             print(returned_info)
             return returned_info # set secure cookie once not in dev
     
 
 @router.post("/logout")
-def logout(response: Response, request: Request, status_code=status.HTTP_200_OK):
+def logout(response: Response):
     response.delete_cookie(
         key="refresh_token",
         httponly=True,
@@ -64,21 +50,13 @@ def logout(response: Response, request: Request, status_code=status.HTTP_200_OK)
 async def refresh_token(request: Request):
     token = request.cookies.get("refresh_token")
     print(f"Refresh token from cookies: {token}")
-    token = token.replace("Bearer ", "")
-    print(token)
     payload = await decode(token, REFRESH_KEY)
     print(payload)
     if token:
-        recentAmendments = asyncio.create_task(getRecentAmendments()) # wraps a async coroutine object into a task
-        # create task is used so that getResolutions() is run right here in the background and we await retrieve the result later
-        # this allows for the rest of the function to run while the task is being executed
         newAccess = generateJwt(payload, SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES)
-        print(payload.get("roles"))
-        if roleList.get("member") in payload.get("roles"):
-            ownAmendments = asyncio.create_task(getOwnAmendments(payload["id"]))
-            return {"accessToken": newAccess, "roles": payload["roles"], "country": payload["country"], "id": payload["id"],"ownAmendments": await ownAmendments, "recentAmendments": await recentAmendments}
-        elif roleList.get("admin") in payload.get("roles"):
-            return {"accessToken": newAccess, "roles": payload["roles"], "country": payload["country"], "recentAmendments": await recentAmendments}
+        recentAmendments = await getRecentAmendments()
+        ownAmendments = await getOwnAmendments(payload["id"])
+        return {"accessToken": newAccess, "role": payload["role"], "country": payload["country"], "id": payload["id"],"ownAmendments": ownAmendments, "recentAmendments": await recentAmendments}
     else:
         raise HTTPException(status_code=401, detail="Missing refresh token")
 
